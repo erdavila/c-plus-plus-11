1d6d3c5cc3805c0b13fbde45716e25319a8ce835
Eduardo R. D'Avila <erdavila@gmail.com>
2015-09-14 22:39:42 -0300

    Implement currying of functions with two arguments

--- curry/curry.cpp
+++ curry/curry.cpp
@@ -49,6 +49,88 @@ namespace oneArg {
 }
 
 
+namespace twoArgs {
+	namespace argsByVal {
+		std::string function(int i, char c) {
+			std::stringstream ss;
+			ss << i << c;
+			return ss.str();
+		}
+
+		void test_1_1() {
+			auto curriedFunction = curry::curry(function);
+			auto curriedFunction2 = curriedFunction(7);
+			auto result = curriedFunction2('!');
+			assert(result == "7!");
+		}
+
+		void test_2() {
+			auto curriedFunction = curry::curry(function);
+			auto result = curriedFunction(7, '!');
+			assert(result == "7!");
+		}
+
+		void test() {
+			test_1_1();
+			test_2();
+		}
+	}
+
+	namespace argsByRef {
+		std::string str;
+
+		std::string& function(int& i, char& c) {
+			std::stringstream ss;
+			ss << i << c;
+			i++;
+			c = '?';
+			str = ss.str();
+			return str;
+		}
+
+		void test_1_1() {
+			auto curriedFunction = curry::curry(function);
+
+			int i = -1;
+			auto curriedFunction2 = curriedFunction(i);
+
+			i = 7;
+			char c = '!';
+			auto& result = curriedFunction2(c);
+
+			assert(i == 8);
+			assert(c == '?');
+			assert(result == "7!");
+			assert(&result == &str);
+		}
+
+		void test_2() {
+			auto curriedFunction = curry::curry(function);
+
+			int i = 7;
+			char c = '!';
+			auto& result = curriedFunction(i, c);
+
+			assert(i == 8);
+			assert(c == '?');
+			assert(result == "7!");
+			assert(&result == &str);
+		}
+
+		void test() {
+			test_1_1();
+			test_2();
+		}
+	}
+
+	void test() {
+		argsByVal::test();
+		argsByRef::test();
+	}
+}
+
+
 int main() {
 	oneArg::test();
+	twoArgs::test();
 }
--- curry/curry.hpp
+++ curry/curry.hpp
@@ -5,6 +5,32 @@
 
 namespace curry {
 
+	template <typename, typename>
+	class BoundFunction;
+
+	template <typename Target, typename Return, typename BoundArg, typename LastArg>
+	class BoundFunction<Target, Return(BoundArg, LastArg)> {
+	private:
+		// Appliable to 2 arguments of types (BoundArg, LastArg)
+		Target target;
+
+		BoundArg boundArg;
+	public:
+		BoundFunction(const Target& target, BoundArg boundArg)
+			: target(target), boundArg(boundArg)
+			{}
+
+		// Application to 1 argument
+		Return operator()(LastArg lastArg) const {
+			return target(boundArg, lastArg);
+		}
+	};
+
+	// Appliable to 2..N arguments of types (NextArg, AfterNextArg, RestArgs...)
+	template <typename Target, typename Return, typename BoundArg, typename NextArg, typename AfterNextArg, typename... RestArgs>
+	class BoundFunction<Target, Return(BoundArg, NextArg, AfterNextArg, RestArgs...)>;
+
+
 	template <typename>
 	class CurriedFunction;
 
@@ -25,9 +51,38 @@ namespace curry {
 		}
 	};
 
-	// Appliable to 2..N arguments of types (Arg1, Arg2, RestArgs...)
 	template <typename Return, typename Arg1, typename Arg2, typename... RestArgs>
-	class CurriedFunction<Return(Arg1, Arg2, RestArgs...)>;
+	class CurriedFunction<Return(Arg1, Arg2, RestArgs...)> {
+	private:
+		using Function = Return(Arg1, Arg2, RestArgs...);
+		using Target = std::function<Function>;
+		using This = CurriedFunction<Function>;
+
+		// Appliable to 2..N arguments of types (Arg1, Arg2, RestArgs...)
+		Target target;
+	public:
+		explicit CurriedFunction(Function function) : target(function) {}
+
+		// Application to 1 argument
+		auto operator()(Arg1 arg1) const {
+			return BoundFunction<This, Function>(*this, arg1);
+		}
+
+		// Application to 2..N-1 arguments
+		template <typename... OtherArgs>
+		auto operator()(Arg1 arg1, Arg2 arg2, OtherArgs&&... otherArgs)
+			-> typename std::enable_if<sizeof...(OtherArgs) < sizeof...(RestArgs),
+			     decltype(this->operator()(arg1)(arg2, std::forward<OtherArgs>(otherArgs)...))
+			   >::type
+		{
+			return        this->operator()(arg1)(arg2, std::forward<OtherArgs>(otherArgs)...);
+		}
+
+		// Application to N arguments
+		Return operator()(Arg1 arg1, Arg2 arg2, RestArgs... restArgs) const {
+			return target(arg1, arg2, restArgs...);
+		}
+	};
 
 
 	template <typename Function>
