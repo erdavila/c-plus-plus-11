761d6d0ec3ceebe09512521a9f924a62b4eefb61
Eduardo R. D'Avila <erdavila@gmail.com>
2015-12-04 23:28:17 -0200

    perfect-forwarding: Include cases for common constructor overloads

--- perfect-forwarding.cpp
+++ perfect-forwarding.cpp
@@ -18,14 +18,19 @@ string cyan(const string& s)    { return "\e[36m" + s + "\e[0m"; }
 struct Result {
 	const string deducedTemplateArg;
 	const string rvalueRefSolvedAs;
-	const string overloadCalled;
+	const string functionOverloadCalled;
+	const string constructorOverloadCalled;
 
-	Result(const string& ded, const string& as, const string& oc)
-		: deducedTemplateArg(ded), rvalueRefSolvedAs(as), overloadCalled(oc)
+	Result(const string& ded, const string& as, const string& foc, const string& coc)
+		: deducedTemplateArg(ded),
+		  rvalueRefSolvedAs(as),
+		  functionOverloadCalled(foc),
+		  constructorOverloadCalled(coc)
 	{}
 };
 bool operator==(const Result& lhs, const Result& rhs) {
-	return lhs.overloadCalled == rhs.overloadCalled
+	return lhs.functionOverloadCalled == rhs.functionOverloadCalled
+	    && lhs.constructorOverloadCalled == rhs.constructorOverloadCalled
 	    && lhs.deducedTemplateArg == rhs.deducedTemplateArg
 	    && lhs.rvalueRefSolvedAs == rhs.rvalueRefSolvedAs;
 }
@@ -42,19 +47,24 @@ DEFINE_TYPEDECL(C);
 string overloadedFunction(const C&) { return "const C&"; }
 string overloadedFunction(C&)       { return "C&"; }
 
+string overloadedConstructor(const C&)  { return "const C&"; }
+string overloadedConstructor(C&&)       { return "C&&"; }
+
 template <typename T>
 Result forwardingFunction(T&& arg) {
 	string deducedTemplateArg = typedecl<T>();
 	string rvalueRefSolvedAs = typedecl<T&&>();
 
-	string overloadCalled = overloadedFunction(forward<T>(arg));
+	string functionOverloadCalled = overloadedFunction(forward<T>(arg));
+	string constructorOverloadCalled = overloadedConstructor(forward<T>(arg));
 
 	cout << "Calling forwardingFunction(" << cyan("T") << yellow("&&") << " arg) with " << blue(arg.name) << endl;
 	cout << "\tDeduced " << cyan("T") << " = " << cyan(deducedTemplateArg) << endl;
 	cout << "\tType of arg is " << cyan("T") << yellow("&&") << " = " << cyan(deducedTemplateArg) << " " << yellow("&&") << " = " << green(rvalueRefSolvedAs) << endl;
-	cout << "\tForwarded to: overloadedFunction(" << red(overloadCalled) << ")" << endl;
+	cout << "\tForwarded to: overloadedFunction(" << red(functionOverloadCalled) << ")" << endl;
+	cout << "\tForwarded to: overloadedConstructor(" << red(constructorOverloadCalled) << ")" << endl;
 	cout << endl;
-	return {deducedTemplateArg, rvalueRefSolvedAs, overloadCalled};
+	return {deducedTemplateArg, rvalueRefSolvedAs, functionOverloadCalled, constructorOverloadCalled};
 }
 
 
@@ -74,12 +84,12 @@ int main() {
 	const C&  const_lref_lvalue = returns<const C&, LVALUE>();
 	const C&& const_rref_lvalue = returns<const C&&, LVALUE>();
 
-	assert(forwardingFunction(lvalue)            == Result(      "C&",       "C&",       "C&"));
-	assert(forwardingFunction(lref_lvalue)       == Result(      "C&",       "C&",       "C&"));
-	assert(forwardingFunction(rref_lvalue)       == Result(      "C&",       "C&",       "C&"));
-	assert(forwardingFunction(const_lvalue)      == Result("const C&", "const C&", "const C&"));
-	assert(forwardingFunction(const_lref_lvalue) == Result("const C&", "const C&", "const C&"));
-	assert(forwardingFunction(const_rref_lvalue) == Result("const C&", "const C&", "const C&"));
+	assert(forwardingFunction(lvalue)            == Result(      "C&",       "C&",       "C&", "const C&"));
+	assert(forwardingFunction(lref_lvalue)       == Result(      "C&",       "C&",       "C&", "const C&"));
+	assert(forwardingFunction(rref_lvalue)       == Result(      "C&",       "C&",       "C&", "const C&"));
+	assert(forwardingFunction(const_lvalue)      == Result("const C&", "const C&", "const C&", "const C&"));
+	assert(forwardingFunction(const_lref_lvalue) == Result("const C&", "const C&", "const C&", "const C&"));
+	assert(forwardingFunction(const_rref_lvalue) == Result("const C&", "const C&", "const C&", "const C&"));
 
 
 	#define            rvalue returns<C, RVALUE>()
@@ -89,12 +99,10 @@ int main() {
 	#define const_lref_rvalue returns<const C&, RVALUE>()
 	#define const_rref_rvalue returns<const C&&, RVALUE>()
 
-	assert(forwardingFunction(           rvalue) == Result(      "C" ,       "C&&", "const C&"));
-	assert(forwardingFunction(      lref_rvalue) == Result(      "C&",       "C&" ,       "C&"));
-	assert(forwardingFunction(      rref_rvalue) == Result(      "C" ,       "C&&", "const C&"));
-	assert(forwardingFunction(     const_rvalue) == Result("const C" , "const C&&", "const C&"));
-	assert(forwardingFunction(const_lref_rvalue) == Result("const C&", "const C&" , "const C&"));
-	assert(forwardingFunction(const_rref_rvalue) == Result("const C" , "const C&&", "const C&"));
-
-	cout << "OK" << endl;
+	assert(forwardingFunction(           rvalue) == Result(      "C" ,       "C&&", "const C&",       "C&&"));
+	assert(forwardingFunction(      lref_rvalue) == Result(      "C&",       "C&" ,       "C&", "const C&"));
+	assert(forwardingFunction(      rref_rvalue) == Result(      "C" ,       "C&&", "const C&",       "C&&"));
+	assert(forwardingFunction(     const_rvalue) == Result("const C" , "const C&&", "const C&", "const C&"));
+	assert(forwardingFunction(const_lref_rvalue) == Result("const C&", "const C&" , "const C&", "const C&"));
+	assert(forwardingFunction(const_rref_rvalue) == Result("const C" , "const C&&", "const C&", "const C&"));
 }
